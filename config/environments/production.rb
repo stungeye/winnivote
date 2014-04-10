623cc22135fd5cd488d34bda6f6ce5e2d4b99777
Kyle Geske <stungeye@gmail.com>
2014-04-10 23:49:37 +0000

    Precompile assets required by heroku.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,7 +47,7 @@ Winnipegrb::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
-
+  config.assets.initialize_on_precompile = true
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
 

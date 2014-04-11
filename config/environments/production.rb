c46ae83ce72a6ad3b3898306fe601ca6da330a1d
Kyle Geske <stungeye@gmail.com>
2014-04-11 00:07:45 +0000

    More forced precompiles for heroku. I think this is likely a bad idea.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,8 +46,8 @@ Winnipegrb::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w( ideas.css )
   config.assets.initialize_on_precompile = true
+  config.assets.precompile += %w( ideas.css welcome.css )
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
 
